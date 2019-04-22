5e37e55c1ec06452cce15a90a47d02f14ed62582
Ethan Zeigler <ethanzeigler@gmail.com>
2019-04-22 14:55:15 -0400

    basic structure of groups, scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :report_groups
   devise_for :users
   root to: 'home#index'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_04_20_024520) do
+ActiveRecord::Schema.define(version: 2019_04_22_175127) do
+
+  create_table "report_groups", force: :cascade do |t|
+    t.text "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "reports", force: :cascade do |t|
     t.text "description"
