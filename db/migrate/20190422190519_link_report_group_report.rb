fd3af5ffffd2044b8f18e7985eee6e497806735e
Ethan Zeigler <ethanzeigler@gmail.com>
2019-04-23 19:14:50 -0400

    fix link table. actually^2

--- db/migrate/20190422190519_link_report_group_report.rb
+++ db/migrate/20190422190519_link_report_group_report.rb
@@ -1,8 +1,8 @@
 class LinkReportGroupReport < ActiveRecord::Migration[5.2]
   def change
     create_join_table :reports, :report_groups do |t|
-      t.int :category_id
-      t.int :user_id
+      t.integer :category_id
+      t.integer :user_id
     end
   end
 end
